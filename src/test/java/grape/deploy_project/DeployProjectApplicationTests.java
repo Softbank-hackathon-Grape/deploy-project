3efc51c690fb3677d9159c69790d37a32e094715
Ji-su-Shin <caujisu2020@gmail.com>
2025-11-07 19:17:49 +0900

    init

---
+++ src/test/java/grape/deploy_project/DeployProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package grape.deploy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
